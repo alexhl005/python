638a84a1d5eadd32aafcf0c2b8efc44ea46530cf
aherrera <herreraalex2005@gmail.com>
2024-11-06 10:54:59 +0100

    modificacion

--- lista/#7.py
+++ lista/#7.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 
 def main(args):
     lista = [(("ASGBD", Decimal(6.5)), ("HLC", Decimal(9))),(("SER", Decimal(8.5)), ("ASO", Decimal(7.5))),(("AIW", Decimal(8)), ("ASGBD", Decimal(6.5)))]
+    print("ASIGNATURAS")
+    print("=============================")
     for i in range(len(lista)):
         print("Alumno", i + 1)
         total = Decimal(0)  # Inicializamos el total en Decimal
